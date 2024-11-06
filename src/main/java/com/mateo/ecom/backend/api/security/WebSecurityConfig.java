d00b0dc6659f0dceb4e71150a7ce987b420b9ac6
mateohysa <hysamateo@gmail.com>
2024-11-06 20:06:55 +0100

    all endpoints (besides products, login and register) now require authentication

--- src/main/java/com/mateo/ecom/backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/mateo/ecom/backend/api/security/WebSecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
 public class WebSecurityConfig {
+    private JWTFilter jwtFilter;
+    public WebSecurityConfig(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,12 +27,13 @@ public class WebSecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
-
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(source))
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/product","auth/login","auth/register").permitAll()
+                        .anyRequest().
+                        authenticated()
                 );
 
         return http.build();
