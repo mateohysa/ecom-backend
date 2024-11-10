c10f939c183f200c715c291be04e688a3eb27abd
mateohysa <hysamateo@gmail.com>
2024-11-10 20:38:44 +0100

    Revert "lay foundation for email verification for users, fixes need to be applied before I move to unit testing"
    This reverts commit 103179423439653f792fd73f2f29c2acb220947b.

--- src/main/java/com/mateo/ecom/backend/api/controller/auth/AuthController.java
+++ src/main/java/com/mateo/ecom/backend/api/controller/auth/AuthController.java
@@ -1,8 +1,6 @@
 package com.mateo.ecom.backend.api.controller.auth;
 
-import com.mateo.ecom.backend.api.exceptions.EmailFailureException;
 import com.mateo.ecom.backend.api.exceptions.UserAlreadyExists;
-import com.mateo.ecom.backend.api.exceptions.UserNotVerifiedException;
 import com.mateo.ecom.backend.api.model.LoginBody;
 import com.mateo.ecom.backend.api.model.LoginResponse;
 import com.mateo.ecom.backend.api.model.RegistrationBody;
@@ -13,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,38 +30,13 @@ public class AuthController {
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExists ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        } catch (EmailFailureException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @PostMapping("/verify")
-    public ResponseEntity verifyEmail(@RequestParam String token){
-        if (userService.verifyUser(token)) {
-            return ResponseEntity.ok().build();
-        }else{
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginBody body) {
-        String jwt = null;
-        try {
-            jwt = userService.logInUser(body);
-        } catch (UserNotVerifiedException e) {
-            LoginResponse loginResponse = new LoginResponse();
-            loginResponse.setSuccess(false);
-            String reason = "USER_NOT_VERIFIED";
-            if (e.isNewEmailSent()){
-                reason += "_EMAIL_RESENT";
-            }
-            loginResponse.setFailureReason(reason);
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(loginResponse);
-        } catch (EmailFailureException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-        if (jwt == null){
+        String jwt = userService.logInUser(body);
+            if (jwt == null){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
             }else {
                 //created this new object so that in the future,
@@ -72,7 +44,6 @@ public class AuthController {
                 //a lot of code change to implement the new details.
                 LoginResponse response = new LoginResponse();
                 response.setToken(jwt);
-                response.setSuccess(true);
                 return ResponseEntity.ok(response);
             }
 
--- src/main/java/com/mateo/ecom/backend/api/exceptions/EmailFailureException.java
+++
@@ -1,5 +0,0 @@
-package com.mateo.ecom.backend.api.exceptions;
-
-public class EmailFailureException extends Exception {
-
-}
