58ec34bc36857cba8999e978557582df2f716d23
mateohysa <hysamateo@gmail.com>
2025-02-16 18:49:32 +0100

    fixed security bug that allowed web-generated JWTs to authenticate as any user

--- src/main/java/com/mateo/ecom/backend/api/model/LoginResponse.java
+++ src/main/java/com/mateo/ecom/backend/api/model/LoginResponse.java
@@ -2,6 +2,8 @@ package com.mateo.ecom.backend.api.model;
 
 public class LoginResponse {
     private String token;
+    private boolean success = true;
+    private String failureMessage;
 
     public String getToken() {
         return token;
@@ -9,5 +11,21 @@ public class LoginResponse {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFailureMessage() {
+        return failureMessage;
+    }
+
+    public void setFailureMessage(String failureMessage) {
+        this.failureMessage = failureMessage;
     }
 }
--- src/main/java/com/mateo/ecom/backend/service/JWTService.java
+++ src/main/java/com/mateo/ecom/backend/service/JWTService.java
@@ -3,6 +3,7 @@ package com.mateo.ecom.backend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mateo.ecom.backend.models.AppUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -52,6 +53,7 @@ public class JWTService {
     //sending requests to our api just by the JWT token
 
     public String findUsername(String token) {
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+        DecodedJWT jwt = JWT.require(algorithm).build().verify(token);
+        return jwt.getClaim(USERNAME_KEY).asString();
     }
 }
